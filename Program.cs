8171f21e8ec4fecd323b993da0b5e33163e11360
Paulo Daniel <pdanielcdo11@gmail.com>
2020-02-06 21:54:17 -0300

    aula 1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppNavegadorPSI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequest req = WebRequest.Create("http://192.168.137.1/Pagina.html");
+
+            WebResponse respo = req.GetResponse();
+
+            StreamReader reader = new StreamReader(respo.GetResponseStream(), Encoding.ASCII);
+
+            Console.WriteLine(reader.ReadToEnd());
+
+            Console.ReadKey();
+        }
+    }
+}
