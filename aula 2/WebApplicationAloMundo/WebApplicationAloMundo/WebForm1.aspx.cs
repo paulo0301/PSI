8926afcd67a180144e523bdfdfcae18cff959ae0
paulo0301 <pdanielcdo11@gmail.com>
2020-02-10 10:34:22 -0300

    aula 2

---
+++ aula 2/WebApplicationAloMundo/WebApplicationAloMundo/WebForm1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplicationAloMundo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Show1a10()
+        {
+            Random rnd = new Random();
+            int qtd = rnd.Next(1, 10);
+
+
+            Response.Write("</br> </br>");
+            for(int i = 1; i <= qtd; i++)
+            {
+                Response.Write(i.ToString() + "</br>");
+            }
+        }
+    }
+}
---
+++ aula 2/WebApplicationCp3/WebApplicationCp3/WebForm1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplicationCp3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p>Página Criada no Visual Studio</p>
+            <asp:Label ID="Label1" runat="server" Text="Digite uma fr"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
